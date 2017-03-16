dc04104819ed006619e110390595d75013406e91
Scott Schroeder <scott19904@gmail.com>
2017-03-15 22:17:17 -0700

    Add CORS for all endpoints

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use std::convert::From;
 extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
-#[macro_use]
+#[macro_use(log,info,debug,warn)]
 extern crate log;
 #[macro_use]
 extern crate error_chain;
@@ -30,6 +30,7 @@ use std::str::FromStr;
 use std::path::PathBuf;
 use rocket::response::NamedFile;
 use rocket_contrib::{JSON, Template, Value};
+use rocket::http::Method;
 use rocket::http::Status;
 use rocket::{Outcome, State};
 use rocket::request::{self, Request, FromRequest};
@@ -42,12 +43,14 @@ type RedisPool = r2d2::Pool<r2d2_redis::RedisConnectionManager>;
 extern crate serde_derive;
 use std::env;
 
+mod cors;
 mod user;
 mod session;
 mod errors;
 mod generator;
 mod redisutil;
 
+use cors::{CORS, PreflightCORS};
 use errors::*;
 use redisutil::RedisBackend;
 
@@ -93,7 +96,6 @@ impl<'r> Responder<'r> for FileLike {
             FileLike::Template(file) => file.respond(),
         }
     }
-
 }
 
 impl<'a, 'r> FromRequest<'a, 'r> for APIKey {
@@ -145,11 +147,8 @@ fn swagger_ui_home() -> Redirect {
 }
 
 #[get("/<file..>")]
-fn swagger_ui(file: PathBuf) -> Result<FileLike> {
-    let templates = vec![
-        "index.html",
-        "swagger.yaml",
-    ];
+fn swagger_ui(file: PathBuf) -> Result<CORS<FileLike>> {
+    let templates = vec!["index.html", "swagger.yaml"];
     for t in templates {
         if PathBuf::from(t) == file {
             info!("Treating {:?} as a template", file.display());
@@ -159,33 +158,33 @@ fn swagger_ui(file: PathBuf) -> Result<FileLike> {
                 hostname_port: match config.get_str("userhost") {
                     Ok(userhost) => userhost.to_string(),
                     Err(_) => format!("{}:{}", config.address, config.port),
-                }
+                },
             };
             info!("Template {} for user host", context.hostname_port);
-            return Ok(FileLike::Template(Some(Template::render(t, &context))))
+            return Ok(FileLike::Template(Some(Template::render(t, &context))).into());
         }
     }
     let fullpath = env::current_dir()?.join("vendor/swagger-ui/").join(file);
     info!("Full Path is: {:?}", fullpath.display());
     let flike = match NamedFile::open(fullpath).ok() {
         Some(file) => FileLike::NamedFile(Some(file)),
-        None => FileLike::NamedFile(None)
+        None => FileLike::NamedFile(None),
     };
-    Ok(flike)
+    Ok(flike.into())
 }
 
 #[get("/<file..>")]
-fn files(file: PathBuf) -> Result<Option<NamedFile>> {
+fn files(file: PathBuf) -> Result<CORS<Option<NamedFile>>> {
     let fullpath = env::current_dir()
         ?
         .join("static/")
         .join(file);
     info!("Full Path is: {:?}", fullpath.display());
-    Ok(NamedFile::open(fullpath).ok())
+    Ok(NamedFile::open(fullpath).ok().into())
 }
 
 #[post("/user", format = "application/json", data = "<name>")]
-fn create_user(name: Option<JSON<NameForm>>, pool: State<RedisPool>) -> Result<JSON<Value>> {
+fn create_user(name: Option<JSON<NameForm>>, pool: State<RedisPool>) -> Result<CORS<JSON<Value>>> {
     let conn = pool.get().unwrap();
 
     info!("Name Object: {:?}", name);
@@ -201,7 +200,8 @@ fn create_user(name: Option<JSON<NameForm>>, pool: State<RedisPool>) -> Result<J
         "user_id": u.user_id,
         "user_token": u.user_token,
         "nickname": u.nickname,
-    })))
+    }))
+        .into())
 }
 
 #[put("/user/<user_id>", format = "application/json", data = "<name>")]
@@ -209,18 +209,18 @@ fn rename_user(user_id: String,
                name: Option<JSON<NameForm>>,
                keys: APIKey,
                pool: State<RedisPool>)
-               -> Result<Option<()>> {
+               -> Result<CORS<Option<()>>> {
     let conn = pool.get().unwrap();
 
     let mut u = match user::User::lookup(&user_id, &conn)? {
         Some(u) => u,
-        None => return Ok(None),
+        None => return Ok(None.into()),
     };
 
     if u.is_authorized(&keys) {
         u.nickname = name.map(|form| form.0.name);
         redisutil::save(&u, &conn)?;
-        Ok(Some(()))
+        Ok(Some(()).into())
     } else {
         bail!(ErrorKind::UserForbidden("Caller is not authorized to delete user".to_owned()))
     }
@@ -233,42 +233,42 @@ fn cast_vote(session_id: String,
              vote: JSON<VoteForm>,
              keys: APIKey,
              pool: State<RedisPool>)
-             -> Result<Option<()>> {
+             -> Result<CORS<Option<()>>> {
     let conn = pool.get().unwrap();
 
     let mut u = match user::User::lookup(&user_id, &conn)? {
         Some(u) => u,
-        None => return Ok(None),
+        None => return Ok(None.into()),
     };
 
     if u.is_authorized(&keys) {
         u.vote(vote.0.vote);
         redisutil::save(&u, &conn)?;
         redisutil::update_session(&session_id, &conn)?;
-        Ok(Some(()))
+        Ok(Some(()).into())
     } else {
         bail!(ErrorKind::UserForbidden("User is not authorized for this user".to_owned()))
     }
 }
 
 #[delete("/user/<user_id>")]
-fn delete_user(user_id: String, keys: APIKey, pool: State<RedisPool>) -> Result<Option<()>> {
+fn delete_user(user_id: String, keys: APIKey, pool: State<RedisPool>) -> Result<CORS<Option<()>>> {
     let conn = pool.get().unwrap();
     let mut u = match user::User::lookup(&user_id, &conn)? {
         Some(u) => u,
-        None => return Ok(None),
+        None => return Ok(None.into()),
     };
 
     if u.is_authorized(&keys) {
         u.delete(&conn)?;
-        Ok(Some(()))
+        Ok(Some(()).into())
     } else {
         bail!(ErrorKind::UserForbidden("Caller is not authorized to delete user".to_owned()))
     }
 }
 
 #[post("/session")]
-fn create_session(pool: State<RedisPool>, keys: APIKey) -> Result<JSON<Value>> {
+fn create_session(pool: State<RedisPool>, keys: APIKey) -> Result<CORS<JSON<Value>>> {
     let conn = pool.get().unwrap();
 
     let s = session::Session::new();
@@ -285,7 +285,8 @@ fn create_session(pool: State<RedisPool>, keys: APIKey) -> Result<JSON<Value>> {
     redisutil::save(&s, &conn)?;
     Ok(JSON(json!({
         "session_id": s.session_id,
-    })))
+    }))
+        .into())
 }
 
 #[post("/session/<session_id>/user/<user_id>", format = "application/json")]
@@ -293,7 +294,7 @@ fn join_session(session_id: String,
                 user_id: String,
                 keys: APIKey,
                 pool: State<RedisPool>)
-                -> Result<()> {
+                -> Result<CORS<()>> {
 
     let conn = pool.get().unwrap();
 
@@ -305,7 +306,7 @@ fn join_session(session_id: String,
     } else {
         bail!(ErrorKind::UserForbidden("Caller is not authorized for this user".to_owned()))
     }
-    Ok(())
+    Ok(().into())
 }
 
 #[delete("/session/<session_id>/user/<user_id>")]
@@ -313,19 +314,19 @@ fn kick_user(session_id: String,
              user_id: String,
              keys: APIKey,
              pool: State<RedisPool>)
-             -> Result<Option<()>> {
+             -> Result<CORS<Option<()>>> {
     let conn = pool.get().unwrap();
 
     let u = match user::User::lookup(&user_id, &conn)? {
         Some(u) => u,
-        None => return Ok(None),
+        None => return Ok(None.into()),
     };
     let s = session::Session::lookup_strict(&session_id, &conn)?;
 
     if u.is_authorized(&keys) || is_admin(&keys, &s, &conn)? {
         s.disassociate(&user_id, "user", &conn)?;
         redisutil::update_session(&session_id, &conn)?;
-        Ok(Some(()))
+        Ok(Some(()).into())
     } else {
         bail!(ErrorKind::UserForbidden("Caller is not authorized to kick user".to_owned()))
     }
@@ -336,13 +337,13 @@ fn grant_admin(session_id: String,
                user_id: String,
                keys: APIKey,
                pool: State<RedisPool>)
-               -> Result<Option<()>> {
+               -> Result<CORS<Option<()>>> {
 
     let conn = pool.get().unwrap();
 
     let s = match session::Session::lookup(&session_id, &conn)? {
         Some(s) => s,
-        None => return Ok(None),
+        None => return Ok(None.into()),
     };
     if is_admin(&keys, &s, &conn)? {
         s.associate(&user_id, "admin", &conn)?;
@@ -352,7 +353,7 @@ fn grant_admin(session_id: String,
             "User is not authorized as session admin".to_owned()
         ))
     }
-    Ok(Some(()))
+    Ok(Some(()).into())
 }
 
 #[delete("/session/<session_id>/admin/<user_id>")]
@@ -360,18 +361,18 @@ fn revoke_admin(session_id: String,
                 user_id: String,
                 keys: APIKey,
                 pool: State<RedisPool>)
-                -> Result<Option<()>> {
+                -> Result<CORS<Option<()>>> {
     let conn = pool.get().unwrap();
 
     let s = match session::Session::lookup(&session_id, &conn)? {
         Some(s) => s,
-        None => return Ok(None),
+        None => return Ok(None.into()),
     };
 
     if is_admin(&keys, &s, &conn)? {
         s.disassociate(&user_id, "admin", &conn)?;
         redisutil::update_session(&session_id, &conn)?;
-        Ok(Some(()))
+        Ok(Some(()).into())
     } else {
         bail!(ErrorKind::UserForbidden(
             "User is not authorized as session admin".to_owned()
@@ -382,18 +383,18 @@ fn revoke_admin(session_id: String,
 #[get("/session/<session_id>")]
 fn lookup_session(session_id: String,
                   pool: State<RedisPool>)
-                  -> Result<Option<JSON<session::PublicSession>>> {
+                  -> Result<CORS<Option<JSON<session::PublicSession>>>> {
     let conn = pool.get().unwrap();
 
     let s = match session::Session::lookup(&session_id, &conn)? {
         Some(s) => s,
-        None => return Ok(None),
+        None => return Ok(None.into()),
     };
 
     let admin_ids = s.get_associates("admin", &conn)?;
     let users = lookup_active_users(&s, &conn)?;
     let public_session = session::PublicSession::new(&s, &users, &admin_ids);
-    Ok(Some(JSON(public_session)))
+    Ok(Some(JSON(public_session)).into())
 }
 
 #[patch("/session/<session_id>", format = "application/json", data = "<state>")]
@@ -401,12 +402,12 @@ fn update_session(session_id: String,
                   state: JSON<SessionStateForm>,
                   keys: APIKey,
                   pool: State<RedisPool>)
-                  -> Result<Option<()>> {
+                  -> Result<CORS<Option<()>>> {
     let conn = pool.get().unwrap();
 
     let mut s = match session::Session::lookup(&session_id, &conn)? {
         Some(s) => s,
-        None => return Ok(None),
+        None => return Ok(None.into()),
     };
 
     if !is_admin(&keys, &s, &conn)? {
@@ -429,16 +430,19 @@ fn update_session(session_id: String,
     }
     redisutil::save(&s, &conn)?;
     redisutil::update_session(&session_id, &conn)?;
-    Ok(Some(()))
+    Ok(Some(()).into())
 }
 
 #[delete("/session/<session_id>")]
-fn delete_session(session_id: String, keys: APIKey, pool: State<RedisPool>) -> Result<Option<()>> {
+fn delete_session(session_id: String,
+                  keys: APIKey,
+                  pool: State<RedisPool>)
+                  -> Result<CORS<Option<()>>> {
     let conn = pool.get().unwrap();
 
     let mut s = match session::Session::lookup(&session_id, &conn)? {
         Some(s) => s,
-        None => return Ok(None),
+        None => return Ok(None.into()),
     };
     if !is_admin(&keys, &s, &conn)? {
         bail!(ErrorKind::UserForbidden(
@@ -447,7 +451,7 @@ fn delete_session(session_id: String, keys: APIKey, pool: State<RedisPool>) -> R
     }
     s.delete(&conn)?;
     redisutil::update_session(&session_id, &conn)?;
-    Ok(Some(()))
+    Ok(Some(()).into())
 }
 
 fn is_admin(keys: &APIKey, session: &session::Session, conn: &redis::Connection) -> Result<bool> {
@@ -462,7 +466,7 @@ fn is_admin(keys: &APIKey, session: &session::Session, conn: &redis::Connection)
     } else {
         false
     };
-    Ok(authorized)
+    Ok(authorized.into())
 }
 
 pub fn lookup_active_users(session: &session::Session,
@@ -478,6 +482,41 @@ pub fn lookup_active_users(session: &session::Session,
     Ok(users)
 }
 
+#[route(OPTIONS, "/<whatever..>")]
+#[allow(unused_variables)] //This is so we match OPTIONS on everything
+fn cors_preflight(whatever: PathBuf) -> PreflightCORS {
+    CORS::preflight("*")
+        .methods(&vec![
+            Method::Options,
+            Method::Get,
+            Method::Post,
+            Method::Put,
+            Method::Delete,
+            Method::Head,
+            Method::Trace,
+            Method::Connect,
+            Method::Patch,
+        ])
+        .headers(&vec!["Content-Type"])
+}
+
+#[error(400)]
+fn handle_error_400() -> CORS<String> {
+    format!("Something was invalid about your request!").into()
+}
+#[error(401)]
+fn handle_error_401() -> CORS<String> {
+    format!("Please provide HTTP Basic Authorization").into()
+}
+#[error(403)]
+fn handle_error_403() -> CORS<String> {
+    format!("Your credentials do not allow you to do this").into()
+}
+
+#[error(404)]
+fn handle_error_404(req: &Request) -> CORS<String> {
+    format!("Unable to locate resource: '{}'", req.uri()).into()
+}
 
 fn main() {
     let cpus = num_cpus::get() as u32;
@@ -494,7 +533,7 @@ fn main() {
     let pool = r2d2::Pool::new(config, manager).unwrap();
 
     rocket::ignite()
-        .mount("/", routes![index,])
+        .mount("/", routes![index, cors_preflight, ])
         .mount("/static", routes![files,])
         .mount("/docs", routes![swagger_ui_home, swagger_ui,])
         .mount("/api",
@@ -511,6 +550,12 @@ fn main() {
             update_session,
             lookup_session,
             delete_session,
+        ])
+        .catch(errors![
+            handle_error_400,
+            handle_error_401,
+            handle_error_403,
+            handle_error_404,
         ])
         .manage(pool)
         .launch();
